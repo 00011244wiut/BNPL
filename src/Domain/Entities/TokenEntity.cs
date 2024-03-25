3420410666dc9118eca3188824cd24afd6308826
00011244wiut <umidjonaskarov@yandex.com>
2024-03-25 15:15:04 +0300

    feat(backend): add token entity
    added token entity to save login token

---
+++ src/Domain/Entities/TokenEntity.cs
@@ -0,0 +1,14 @@
+using Domain.Common;
+
+namespace Domain.Entities;
+
+public class TokenEntity : BaseEntity<Guid>
+{
+    public string RefreshToken { get; set; } = string.Empty;
+
+    public DateTime Expires { get; set; } = DateTime.UtcNow.AddDays(7);
+
+    public Guid UserId { get; set; }
+    public UserEntity User { get; set; } = null!;
+}
+
