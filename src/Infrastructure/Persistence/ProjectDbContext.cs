129ab6cc86cade828e49e017dd1df280a8356e04
00011244wiut <umidjonaskarov@yandex.com>
2024-03-25 15:23:42 +0300

    feat(backend): define DbContext
    create a dbContext where every entity is defined to access the database through the orm

--- src/Infrastructure/Persistence/ProjectDbContext.cs
+++ src/Infrastructure/Persistence/ProjectDbContext.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
 public class ProjectDbContext : DbContext
 {
-    public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) { }
+    public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options) {  }
+    
+    public required DbSet<TokenEntity> Tokens { get; set; }
+    public required DbSet<UserEntity> Users { get; set; }
+    public required DbSet<SimulationEntity> Simulations { get; set; }
+    public required DbSet<SchedulesEntity> Schedules { get; set; }
+    public required DbSet<SalesEntity> Sales { get; set; }
+    public required DbSet<PurchaseEntity> Purchases { get; set; }
+    public required DbSet<PurchaseLimitEntity> PurchaseLimits { get; set; }
+    public required DbSet<ProductsEntity> Products { get; set; }
+    public required DbSet<PaymentsEntity> Payments { get; set; }
+    public required DbSet<MerchantEntity> Merchants { get; set; }
+    public required DbSet<MerchantDocumentsEntity> MerchantDocuments { get; set; }
+    public required DbSet<LegalDataEntity> LegalData { get; set; }
+    public required DbSet<CardsEntity> Cards { get; set; }
+    public required DbSet<BankInfoEntity> BankInfo { get; set; }
 }
