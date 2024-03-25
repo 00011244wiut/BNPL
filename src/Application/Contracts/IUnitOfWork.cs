538f01d2a5cdf96eea60ccc1a0215d640ff48732
00011244wiut <umidjonaskarov@yandex.com>
2024-03-25 15:28:44 +0300

    feat(backend): define unitofwork infrastrucutre
    define unitofwork

---
+++ src/Application/Contracts/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.Repositories;
+
+namespace Application.Contracts;
+
+public interface IUnitOfWork : IDisposable
+{
+    ITokenRepository TokenRepository { get; }
+    IUserRepository UserRepository { get; }
+    IUserDocumentsRepository UserDocumentsRepository { get; }
+    ISimulationRepository SimulationRepository { get; }
+    ISchedulesRepository SchedulesRepository { get; }
+    ISalesRepository SalesRepository { get; }
+    IPurchaseRepository PurchaseRepository { get; }
+    IPurchaseLimitRepository PurchaseLimitRepository { get; }
+    IProductsRepository ProductsRepository { get; }
+    IPaymentsRepository PaymentsRepository { get; }
+    IMerchantRepository MerchantRepository { get; }
+    IMerchantDocumentsRepository MerchantDocumentsRepository { get; }
+    ILegalDataRepository LegalDataRepository { get; }
+    ICardsRepository CardsRepository { get; }
+    IBankInfoRepository BankInfoRepository { get; }
+    
+    Task CommitAsync();
+
+}
