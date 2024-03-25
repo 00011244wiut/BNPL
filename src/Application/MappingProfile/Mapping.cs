791d67d938700ec05cfdd36114b51d378bdd1c1f
00011244wiut <umidjonaskarov@yandex.com>
2024-03-25 15:21:59 +0300

    feat(backend): define DbContext
    create a dbContext where every entity is defined to access the database through the orm

---
+++ src/Application/MappingProfile/Mapping.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Auth;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfile;
+
+public class Mapping : Profile
+{
+    public Mapping()
+    {
+        CreateMap<UserEntity, UserResponseDto>().ReverseMap();
+    }
+}
