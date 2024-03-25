893caa45ab779b03608fd73966bd09297c49d002
00011244wiut <umidjonaskarov@yandex.com>
2024-03-25 15:34:10 +0300

    feat(backend): define endpoints for the methods
    created an endpoint for the methods created

---
+++ src/Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Auth.Commands.SignUpSignIn;
+using Application.Features.Auth.Commands.VerifyOtp;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> SignUpSignIn(string phoneNumber)
+    {
+        var response = await _mediator.Send(new SignUpSignInCommand(phoneNumber));
+        return Ok(response);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> VerifyOTP(string phoneNumber, string sampleOtp)
+    {
+        var response = await _mediator.Send(new VerifyOtpCommand(phoneNumber, sampleOtp));
+        return Ok(response);
+    }
+}
