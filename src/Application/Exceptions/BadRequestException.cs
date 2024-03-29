9162f3c6c64e7226780b26180731feac8db288f3
00011244wiut <umidjonaskarov@yandex.com>
2024-03-29 16:06:54 +0300

    feat(backend): add comments on Exception

--- src/Application/Exceptions/BadRequestException.cs
+++ src/Application/Exceptions/BadRequestException.cs
@@ -1,8 +1,12 @@
 namespace Application.Exceptions;
 
+// Exception class representing a bad request with validation errors.
 public class BadRequestException : Exception
 {
+    // Dictionary containing validation errors.
     public IDictionary<string, string[]> ValidationErrors { get; set; } = null!;
+
+    // Constructor with message parameter.
     public BadRequestException(string message) : base(message)
     { }
 }
--- src/Application/Exceptions/NotFoundException.cs
+++ src/Application/Exceptions/NotFoundException.cs
@@ -1,7 +1,9 @@
 namespace Application.Exceptions;
 
+// Exception class representing a not found error.
 public class NotFoundException : Exception
 {
+    // Constructor with message parameter.
     public NotFoundException(string message) : base(message)
     { }
 }
