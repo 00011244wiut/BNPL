9ccb4a5ae966041871422c40c5bf76a422c21a52
00011244wiut <umidjonaskarov@yandex.com>
2024-04-28 22:28:49 +0300

    feat(backend): input validations

--- src/Application/Features/Merchant/Commands/RegisterMerchantName/RegisterMerchantNameValidator.cs
+++ src/Application/Features/Merchant/Commands/RegisterMerchantName/RegisterMerchantNameValidator.cs
@@ -7,6 +7,16 @@ public class RegisterMerchantNameValidator : AbstractValidator<RegisterMerchantN
     // Constructor for RegisterMerchantNameValidator
     public RegisterMerchantNameValidator()
     {
-        // Validation rules for RegisterMerchantNameCommand can be added here if needed
+        // Rule for FirstName
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First Name is required")
+            .Matches("^[A-Z][a-zA-Z]*$")
+            .WithMessage("First Name must start with a capital letter and only contain English alphabet letters.");
+        
+        // Rule for LastName
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last Name is required")
+            .Matches("^[A-Z][a-zA-Z]*$")
+            .WithMessage("Last Name must start with a capital letter and only contain English alphabet letters.");
     }
 }
--- src/Application/Features/User/Commands/UserScore/UserScoreValidator.cs
+++ src/Application/Features/User/Commands/UserScore/UserScoreValidator.cs
@@ -6,6 +6,9 @@ public class UserScoreValidator : AbstractValidator<UserScoreCommand>
 {
     public UserScoreValidator()
     {
-        
+        // Rule for income Enter Income value between 1000000 UZS and 25000000 UZS
+        RuleFor(x => x.Income)
+            .InclusiveBetween(1000000, 25000000)
+            .WithMessage("Enter Income value between 1000000 UZS and 25000000 UZS");
     }
 }
